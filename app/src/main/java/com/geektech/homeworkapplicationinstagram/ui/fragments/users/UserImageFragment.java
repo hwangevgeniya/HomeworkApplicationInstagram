516e9eade25794cdadd01f443b6779e8327873ed
hwangevgeniya <hwang.evgeniya@gmail.com>
2021-09-09 00:07:50 +0600

    Refresh

---
+++ app/src/main/java/com/geektech/homeworkapplicationinstagram/ui/fragments/users/UserImageFragment.java
@@ -0,0 +1,22 @@
+package com.geektech.homeworkapplicationinstagram.ui.fragments.users;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.geektech.homeworkapplicationinstagram.R;
+
+
+public class UserImageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_image, container, false);
+    }
+}
